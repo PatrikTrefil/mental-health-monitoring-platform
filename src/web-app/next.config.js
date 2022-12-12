179089bcccc815fc0c382879da46fadbb742b213
Patrik Trefil <patrik.trefil@gmail.com>
2022-12-12 14:29:00 +0100

    fix hot reloading

--- src/web-app/next.config.js
+++ src/web-app/next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
