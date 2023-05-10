7edff2e17adce1574f3d332cd5337e37aac06665
Patrik Trefil <patrik.trefil@gmail.com>
2023-05-10 09:34:40 +0200

    register new patient/client page

---
+++ src/web-app/pages/zamestnanec/registrace-pacienta-klienta.tsx
@@ -0,0 +1,16 @@
+import WithAuth from "../../components/WithAuth";
+import DynamicForm from "../../components/dynamicFormio/DynamicForm";
+import { UserRoleTitles } from "../../redux/users";
+
+export default WithAuth(
+    <RegistracePacientaKlientaPage />,
+    "/zamestnanec/login",
+    UserRoleTitles.ZAMESTNANEC
+);
+
+/**
+ * Page for registering a new patient/client.
+ */
+function RegistracePacientaKlientaPage() {
+    return <DynamicForm src="/klientpacient/register" />;
+}
