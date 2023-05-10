36e2fa3045e864de15d8d27d1a501c96c1f3b83a
Patrik Trefil <patrik.trefil@gmail.com>
2023-05-10 09:29:55 +0200

    list of forms in user dashboard

--- src/web-app/pages/prehled.tsx
+++ src/web-app/pages/prehled.tsx
@@ -3,6 +3,8 @@ import CurrentUserDetails from "../components/CurrentUserDetails";
 import WithAuth from "../components/WithAuth";
 import { UserRoleTitles } from "../redux/users";
 import Head from "next/head";
+import ClientPatientFillableFormList from "../components/ClientPatientFillableFormList";
+import { FormLineProps } from "../components/FormList";
 
 export default WithAuth(
     <PrehledPage />,
@@ -23,7 +25,23 @@ function PrehledPage() {
                 <h1>Přehled</h1>
                 <CurrentUserDetails />
                 <LogoutButton />
+                <h2>Seznam formulářů k vyplnění</h2>
+                <ClientPatientFillableFormList FormLine={UserFormLine} />
             </main>
         </>
+    );
+}
+
+/**
+ * List item with functionality for a user with the role "Klient/Pacient".
+ *
+ * It contains a link to fill the form.
+ */
+function UserFormLine({ form }: FormLineProps) {
+    return (
+        <li>
+            Název: {`"${form.name}"`}{" "}
+            <a href={`/formular/vyplnit/${form._id}`}>vyplnit</a>
+        </li>
     );
 }
