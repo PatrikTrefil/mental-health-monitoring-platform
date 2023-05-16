29988370b50fb8fb7fa783583f3a226205ff5c47
Patrik Trefil <patrik.trefil@gmail.com>
2023-05-16 08:42:50 +0200

    proper error handling in formular/upravit

--- src/web-app/pages/formular/upravit/[formId].tsx
+++ src/web-app/pages/formular/upravit/[formId].tsx
@@ -5,6 +5,15 @@ import { UserRoleTitles } from "@/redux/users";
 import { Form } from "@/types/form";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    ModalHeader,
+    ModalTitle,
+} from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
 import Spinner from "react-bootstrap/Spinner";
 
 export default WithAuth(
@@ -14,20 +23,46 @@ export default WithAuth(
 );
 
 /**
+ * Status of form edit
+ */
+enum EditStatus {
+    /**
+     * User has not attempted to save form yet (or has reset the status)
+     */
+    NOT_SUBMITTED,
+    /**
+     * User has attempted to save form, but saving failed.
+     */
+    EDIT_FAILED,
+    /**
+     * User has attempted to save form, and saving succeeded.
+     */
+    EDIT_SUCCEEDED,
+}
+
+/**
  * Page for editing form with given form id (from url)
  */
 function EditFormPage() {
     const router = useRouter();
     const [form, setForm] = useState<Form | null>(null);
+    const [editStatus, setEditStatus] = useState<EditStatus>(
+        EditStatus.NOT_SUBMITTED
+    );
+    const [initFormError, setInitFormError] = useState<string | null>(null);
 
     useEffect(
         function initForm() {
             const getForm = async () => {
                 if (router.query.formId === undefined) return;
 
-                // TODO: show error to user instead of throwing
-                if (typeof router.query.formId !== "string")
-                    throw new Error("formId is not a string");
+                if (typeof router.query.formId !== "string") {
+                    setInitFormError(
+                        `Chyba: id formuláře musí byt string (nyní má typ ${typeof router
+                            .query.formId})`
+                    );
+                    return;
+                }
 
                 const { Formio } = await import("formiojs");
 
@@ -41,8 +76,8 @@ function EditFormPage() {
                 try {
                     newForm = await client.loadForm();
                 } catch (e) {
-                    // TODO: show error to user instead of throwing
-                    throw e;
+                    setInitFormError(`Chyba: načítání formuláře selhalo: ${e}`);
+                    return;
                 }
                 setForm(newForm);
             };
@@ -51,28 +86,76 @@ function EditFormPage() {
         [router]
     );
 
-    if (form === null)
+    const formInitFailed = initFormError !== null;
+    if (formInitFailed)
         return (
-            <Spinner animation="border" role="status">
-                <span className="visually-hidden">Načítání...</span>
-            </Spinner>
+            <>
+                <Alert variant="danger">{initFormError}</Alert>
+                <div className="d-flex flex-wrap gap-2">
+                    <Button onClick={() => router.reload()}>
+                        Znovu načíst stránku
+                    </Button>
+                    <Button href="/zamestnanec/prehled">Zpět na přehled</Button>
+                </div>
+            </>
         );
-    else
+
+    const isFormLoading = form === null;
+    if (isFormLoading)
         return (
+            <div className="position-absolute top-50 start-50 translate-middle">
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Načítání...</span>
+                </Spinner>
+            </div>
+        );
+
+    return (
+        <>
             <DynamicFormEdit
                 saveText="Uložit formulář"
                 saveForm={(formSchema: unknown) => {
-                    // TODO: show error to user instead of throwing
-                    saveFormToServer(formSchema);
-                    // TODO: show message box with message and button to go to Prehled instead
-                    alert("Formulář byl úspěšně uložen");
-                    router.push("/zamestnanec/prehled");
+                    try {
+                        saveFormToServer(formSchema);
+                    } catch (e) {
+                        setEditStatus(EditStatus.EDIT_FAILED);
+                    }
+                    setEditStatus(EditStatus.EDIT_SUCCEEDED);
                 }}
                 form={form}
             >
                 <DynamicFormBuilder />
             </DynamicFormEdit>
-        );
+            <Modal show={editStatus === EditStatus.EDIT_FAILED}>
+                <ModalHeader>
+                    <ModalTitle>Úprava selhala</ModalTitle>
+                </ModalHeader>
+                <ModalBody>Odeslání upraveného formuláře selhalo</ModalBody>
+                <ModalFooter>
+                    <Button
+                        variant="secondary"
+                        onClick={() => setEditStatus(EditStatus.NOT_SUBMITTED)}
+                    >
+                        Zavřít
+                    </Button>
+                </ModalFooter>
+            </Modal>
+            <Modal show={editStatus === EditStatus.EDIT_SUCCEEDED}>
+                <ModalHeader>
+                    <ModalTitle>Formulář upraven</ModalTitle>
+                </ModalHeader>
+                <ModalBody>Formulář byl úspěšně upraven.</ModalBody>
+                <ModalFooter>
+                    <Button
+                        variant="secondary"
+                        onClick={() => router.push("/zamestnanec/prehled")}
+                    >
+                        Zavřít
+                    </Button>
+                </ModalFooter>
+            </Modal>
+        </>
+    );
 }
 
 /**
