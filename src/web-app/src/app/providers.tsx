10fbef39608109350ffec0e297d4be0cf20e5f5e
Patrik Trefil <patrik.trefil@gmail.com>
2023-06-08 14:37:35 +0200

    fix toastify

--- src/web-app/src/app/providers.tsx
+++ src/web-app/src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { ClientProvider as TrpcClientProvider } from "@/client/trpcClient";
 import { SSRProvider } from "@/components/shared/client-react-bootstrap";
 import { SessionProvider } from "next-auth/react";
+import { ToastContainer } from "react-toastify";
 
 // react query provider is in the trpc client provider
 
@@ -10,7 +11,16 @@ export function Providers({ children }: { children?: React.ReactNode }) {
     return (
         <TrpcClientProvider>
             <SSRProvider>
-                <SessionProvider>{children}</SessionProvider>
+                <SessionProvider>
+                    {children}
+                    {/* Provides toast container */}
+                    <ToastContainer
+                        pauseOnFocusLoss={false}
+                        autoClose={false}
+                        position="bottom-right"
+                        theme="colored"
+                    />
+                </SessionProvider>
             </SSRProvider>
         </TrpcClientProvider>
     );
