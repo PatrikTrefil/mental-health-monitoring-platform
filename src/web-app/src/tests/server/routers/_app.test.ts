16cdfbfbb288bfac5abc51fcf850c26f197ce805
Patrik Trefil <patrik.trefil@gmail.com>
2023-06-12 14:28:16 +0200

    fix snapshot

--- src/web-app/src/tests/server/routers/_app.test.ts
+++ src/web-app/src/tests/server/routers/_app.test.ts
@@ -364,6 +364,6 @@ describe("todo when formio is down", () => {
         );
         await expect(
             caller.createTask(mockInputTask)
-        ).rejects.toMatchInlineSnapshot("[TRPCError: INTERNAL_SERVER_ERROR]");
+        ).rejects.toMatchInlineSnapshot('[TRPCError: formio is down]');
     });
 });
