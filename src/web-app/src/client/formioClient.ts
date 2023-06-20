3900c011ed5a6d0d21153c7ca7df0b42e71792e3
Patrik Trefil <patrik.trefil@gmail.com>
2023-06-20 12:50:54 +0200

    move formio abstractions to formio client form auth

--- src/web-app/src/client/formioClient.ts
+++ src/web-app/src/client/formioClient.ts
@@ -296,3 +296,144 @@ export async function updateForm(formSchema: Form, formioToken: string) {
             `Failed to save form with status code ${response.status}`
         );
 }
+
+/**
+ * Fetches the list of roles from the server.
+ * @param adminToken JWT token with admin privileges
+ * @returns list of roles
+ * @throws RequestError if the request's status is not ok
+ * @throws TypeError if the request completely fails
+ */
+export async function fetchRoleList(adminToken: string) {
+    console.log("Fetching role list...");
+    const roleListResponse = await fetch(
+        `${process.env.FORMIO_SERVER_URL}/role`,
+        {
+            headers: {
+                "x-jwt-token": adminToken,
+            },
+        }
+    );
+    console.log("Role list fetched.", {
+        status: roleListResponse.status,
+    });
+    if (!roleListResponse.ok) throw new RequestError(roleListResponse.status);
+
+    const roleList = (await roleListResponse.json()) as {
+        _id: string;
+        title: string;
+    }[];
+
+    return roleList;
+}
+/**
+ * Logs in the admin account and returns the token.
+ * @param email email of the admin account
+ * @param password password of the admin account
+ * @returns JWT token with admin privileges
+ * @throws RequestError if the request's status is not ok
+ * @throws TypeError if the request completely fails
+ */
+export async function loginAdmin(email: string, password: string) {
+    console.log("Making admin login request...");
+    const adminLoginResponse = await fetch(
+        `${process.env.FORMIO_SERVER_URL}/user/login`,
+        {
+            body: JSON.stringify({
+                data: {
+                    email,
+                    password,
+                },
+            }),
+            headers: {
+                "Content-type": "application/json; charset=utf-8",
+            },
+            method: "POST",
+        }
+    );
+    console.log("Admin login request done.", {
+        status: adminLoginResponse.status,
+    });
+
+    if (!adminLoginResponse.ok)
+        throw new RequestError(adminLoginResponse.status);
+
+    const adminToken = adminLoginResponse.headers.get("x-jwt-token");
+
+    if (!adminToken) throw new Error("No token received");
+
+    return adminToken;
+}
+/**
+ * Refreshes the token of a user.
+ * @param currentToken current JWT token of the user
+ * @throws RequestError if the request's status is not ok
+ * @throws TypeError if the request completely fails
+ */
+export async function refreshToken(currentToken: string) {
+    console.log("Refreshing token...");
+    const refreshResponse = await fetch(
+        `${process.env.FORMIO_SERVER_URL}/current`,
+        {
+            headers: {
+                "x-jwt-token": currentToken,
+            },
+        }
+    );
+    if (!refreshResponse.ok) throw new RequestError(refreshResponse.status);
+    const newToken = refreshResponse.headers.get("x-jwt-token");
+    if (!newToken) throw new Error("No token received");
+    console.log("Token refreshed.");
+    return newToken;
+}
+/**
+ * Log in as a user
+ * @param id id of the user
+ * @param password password of the user
+ * @returns user and JWT token
+ * @throws RequestError if the request's status is not ok
+ * @throws TypeError if the request completely fails
+ */
+export async function loginUser(id: string, password: string) {
+    console.log("Making login request...");
+    const loginResponse = await fetch(
+        `${process.env.FORMIO_SERVER_URL}/login`,
+        {
+            body: JSON.stringify({
+                data: {
+                    id,
+                    password,
+                },
+            }),
+            headers: {
+                "Content-type": "application/json; charset=utf-8",
+            },
+            method: "POST",
+        }
+    );
+    console.log("Login request done.", {
+        status: loginResponse.status,
+    });
+
+    if (!loginResponse.ok) throw new RequestError(loginResponse.status);
+
+    const user = (await loginResponse.json()) as UserFormSubmission;
+    const formioToken = loginResponse.headers.get("x-jwt-token");
+    if (!formioToken) throw new Error("No token received");
+
+    return { user, formioToken };
+}
+
+/**
+ * Error thrown when a fetch request fails.
+ */
+export class RequestError extends Error {
+    /**
+     * Status code of the failed request.
+     */
+    statusCode: number;
+    constructor(status: number) {
+        super();
+        this.statusCode = status;
+    }
+}
