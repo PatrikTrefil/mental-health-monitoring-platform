28db3fcce515ac10e01dd19f56234f3208b540ef
Patrik Trefil <patrik.trefil@gmail.com>
2023-07-11 17:53:15 +0200

    task router with mock db

--- src/web-app/src/tests/server/routers/taskRouter.test.ts
+++ src/web-app/src/tests/server/routers/taskRouter.test.ts
@@ -2,8 +2,11 @@ import { loadFormById } from "@/client/formManagementClient";
 import { loadUsers } from "@/client/userManagementClient";
 import TaskState from "@/constants/taskState";
 import UserRoleTitles from "@/constants/userRoleTitles";
+import { prisma } from "@/server/__mocks__/db";
 import { appRouter, type AppRouter } from "@/server/routers/root";
-import { type inferProcedureInput } from "@trpc/server";
+import { faker } from "@faker-js/faker";
+import { Prisma } from "@prisma/client";
+import { inferProcedureOutput, type inferProcedureInput } from "@trpc/server";
 import { describe, expect, it, vi } from "vitest";
 import {
     createInnerTRPCContextMockSession,
@@ -12,11 +15,22 @@ import {
 
 type CreateTaskInput = inferProcedureInput<AppRouter["task"]["createTask"]>;
 const mockInputTask: CreateTaskInput & { description: string } = {
-    name: "test",
-    forUserId: "123",
-    description: "test",
-    formId: "123",
+    name: "foo",
+    forUserId: faker.string.uuid(),
+    description: "bar",
+    formId: faker.string.uuid(),
 };
+const mockOutputTaskExpectationTemplate = {
+    ...mockInputTask,
+    id: expect.any(String),
+    createdAt: expect.any(Date),
+    updatedAt: expect.any(Date),
+    createdByEmployeeId: expect.any(String),
+    state: TaskState.READY,
+    submissionId: null,
+};
+
+vi.mock("@/server/db");
 
 vi.mock("@/client/formManagementClient", () => ({
     loadFormById: vi.fn(async () => {
@@ -53,11 +67,10 @@ vi.mock("@/client/userManagementClient", () => ({
 
 describe("todo functionality", () => {
     it("returns created todo as employee", async () => {
-        const caller = appRouter.createCaller(
-            createInnerTRPCContextMockSession([
-                UserRoleTitles.ZADAVATEL_DOTAZNIKU,
-            ])
-        );
+        const employeeCtx = createInnerTRPCContextMockSession([
+            UserRoleTitles.ZADAVATEL_DOTAZNIKU,
+        ]);
+        const caller = appRouter.createCaller(employeeCtx);
 
         vi.mocked(loadFormById).mockResolvedValueOnce({
             _id: mockInputTask.formId,
@@ -68,35 +81,55 @@ describe("todo functionality", () => {
             submissionAccess: [],
             components: [],
         });
+        vi.mocked(loadUsers).mockResolvedValueOnce([
+            {
+                _id: faker.string.uuid(),
+                data: { id: mockInputTask.forUserId },
+                access: [],
+                created: faker.date.past().toISOString(),
+                form: faker.string.uuid(),
+                metadata: {},
+                owner: faker.string.uuid(),
+                roles: [],
+            },
+        ]);
+        if (!employeeCtx.session) throw new Error("no session");
+
+        prisma.task.create.mockResolvedValueOnce(
+            mockOutputTaskExpectationTemplate
+        );
         const createdTask = await caller.task.createTask(mockInputTask);
 
-        expect(createdTask).toMatchObject({
-            ...mockInputTask,
-            id: expect.any(String),
-        });
+        expect(createdTask).toMatchObject(mockOutputTaskExpectationTemplate);
     });
 
     it("get existing todo as patient", async () => {
-        const employeeCtx = createInnerTRPCContextMockSession([
-            UserRoleTitles.ZADAVATEL_DOTAZNIKU,
-        ]);
-        const employeeCaller = appRouter.createCaller(employeeCtx);
-
-        const createdTask = await employeeCaller.task.createTask(mockInputTask);
-
-        const clientCtx = createInnerTRPCContextMockSession([
-            UserRoleTitles.KLIENT_PACIENT,
-        ]);
+        // arrange
+        const clientCtx = createInnerTRPCContextMockSession(
+            [UserRoleTitles.KLIENT_PACIENT],
+            mockInputTask.forUserId
+        );
         const clientCaller = appRouter.createCaller(clientCtx);
 
+        if (!clientCtx.session) throw new Error("no session");
+
+        // act
+        const mockTaskId = faker.string.uuid();
+        prisma.task.findUnique.mockResolvedValueOnce({
+            ...mockInputTask,
+            id: mockTaskId,
+            createdAt: faker.date.past(),
+            updatedAt: faker.date.past(),
+            createdByEmployeeId: faker.string.uuid(),
+            state: TaskState.READY,
+            submissionId: null,
+        });
         const receivedTask = await clientCaller.task.getTask({
-            id: createdTask.id,
+            id: mockTaskId,
         });
 
-        expect(receivedTask).toMatchObject({
-            ...mockInputTask,
-            id: expect.any(String),
-        });
+        // assert
+        expect(receivedTask).toMatchObject(mockOutputTaskExpectationTemplate);
     });
 
     it("throws when getting non-existing todo as employee", async () => {
@@ -105,11 +138,14 @@ describe("todo functionality", () => {
                 UserRoleTitles.ZADAVATEL_DOTAZNIKU,
             ])
         );
-        const todoId = "123";
-        // delete if already exists
-        try {
-            await caller.task.deleteTask({ id: todoId });
-        } catch (e) {}
+        const todoId = faker.string.uuid();
+
+        prisma.task.findUnique.mockImplementationOnce(() => {
+            throw new Prisma.PrismaClientKnownRequestError("Not found", {
+                code: "P2025",
+                clientVersion: "",
+            });
+        });
 
         expect(() =>
             caller.task.getTask({ id: todoId })
@@ -122,15 +158,23 @@ describe("todo functionality", () => {
         ]);
         const caller = appRouter.createCaller(ctx);
 
+        if (!ctx.session) throw new Error("no session");
         const numberOfTasks = 10;
-        // create tasks to list
+        const mockTasks = [];
         for (let i = 0; i < numberOfTasks; i++) {
-            await caller.task.createTask({
+            mockTasks.push({
                 ...mockInputTask,
                 name: `test ${i}`,
+                id: faker.string.uuid(),
+                updatedAt: faker.date.past(),
+                createdByEmployeeId: ctx.session.user.data.id,
+                createdAt: faker.date.past(),
+                state: TaskState.READY,
+                submissionId: null,
             });
         }
 
+        prisma.task.findMany.mockResolvedValueOnce(mockTasks);
         const tasks = await caller.task.listTasks();
 
         if (!ctx.session?.user.data.id) throw new Error("Session is null");
@@ -155,73 +199,39 @@ describe("todo functionality", () => {
 
     it("lists my todos as client/patient", async () => {
         const numberOfTasks = 10;
-        const patientId = "12345";
-        const createdTasks = [];
-        // create tasks for patient
-        {
-            const employeeCtx = createInnerTRPCContextMockSession([
-                UserRoleTitles.ZADAVATEL_DOTAZNIKU,
-            ]);
-            const employeeCaller = appRouter.createCaller(employeeCtx);
-
-            // create tasks to list
-            for (let i = 0; i < numberOfTasks; i++) {
-                vi.mocked(loadUsers).mockImplementationOnce(async () => {
-                    const mockUsers = [
-                        {
-                            _id: "12345",
-                            data: { id: patientId },
-                            created: "",
-                            owner: "",
-                            access: [],
-                            form: "",
-                            roles: [],
-                            metadata: {},
-                        },
-                    ];
-                    return mockUsers;
-                });
-                createdTasks.push(
-                    await employeeCaller.task.createTask({
-                        ...mockInputTask,
-                        name: `test ${i}`,
-                        forUserId: patientId,
-                    })
-                );
-            }
+        const patientId = faker.string.uuid();
+        const createdTasks: inferProcedureOutput<
+            AppRouter["task"]["listTasks"]
+        > = new Array(numberOfTasks);
+        for (let i = 0; i < numberOfTasks; i++) {
+            createdTasks[i] = {
+                ...mockInputTask,
+                name: `test ${i}`,
+                forUserId:
+                    i > numberOfTasks / 2 ? patientId : faker.string.uuid(), // make some tasks for other users
+                createdAt: faker.date.past(),
+                updatedAt: faker.date.past(),
+                createdByEmployeeId: faker.string.uuid(),
+                state: TaskState.READY,
+                submissionId: null,
+                description: "",
+                id: faker.string.uuid(),
+            };
         }
         // check that all tasks that were created are listed
-        {
-            const patientCtx = createInnerTRPCContextMockSession(
-                [UserRoleTitles.KLIENT_PACIENT],
-                patientId
-            );
-            const patientCaller = appRouter.createCaller(patientCtx);
-            const patientsTasks = await patientCaller.task.listTasks();
-
-            // check that all tasks are present in the returned list
-            for (let i = 0; i < numberOfTasks; i++) {
-                expect(patientsTasks).toContainEqual(createdTasks[i]);
-            }
-        }
-    });
-
-    it("deletes an existing todo as employee", async () => {
-        const caller = appRouter.createCaller(
-            createInnerTRPCContextMockSession([
-                UserRoleTitles.ZADAVATEL_DOTAZNIKU,
-            ])
+        const patientCtx = createInnerTRPCContextMockSession(
+            [UserRoleTitles.KLIENT_PACIENT],
+            patientId
         );
+        const patientCaller = appRouter.createCaller(patientCtx);
+        prisma.task.findMany.mockResolvedValueOnce(createdTasks);
+        const patientsTasks = await patientCaller.task.listTasks();
 
-        const createdTask = await caller.task.createTask(mockInputTask);
-
-        await caller.task.deleteTask({
-            id: createdTask.id,
-        });
-
-        expect(
-            caller.task.getTask({ id: createdTask.id })
-        ).rejects.toMatchInlineSnapshot("[TRPCError: NOT_FOUND]");
+        // assert
+        for (const createdTask of createdTasks) {
+            if (createdTask.forUserId === patientId)
+                expect(patientsTasks).toContainEqual(createdTask);
+        }
     });
 
     it("throws not found when deleting a non-existing todo as employee", async () => {
@@ -231,16 +241,15 @@ describe("todo functionality", () => {
             ])
         );
 
-        // delete if already exists
-        try {
-            await caller.task.deleteTask({
-                id: "20",
+        prisma.task.delete.mockImplementationOnce(() => {
+            throw new Prisma.PrismaClientKnownRequestError("Not found", {
+                code: "P2025",
+                clientVersion: "",
             });
-        } catch (e) {}
-
+        });
         await expect(
             caller.task.deleteTask({
-                id: "20",
+                id: faker.string.uuid(),
             })
         ).rejects.toMatchInlineSnapshot("[TRPCError: NOT_FOUND]");
     });
@@ -276,18 +285,12 @@ describe("todo functionality", () => {
 
 describe("todo permissions", () => {
     it("throws when deleting a todo as a client/patient", async () => {
-        const employeeCaller = appRouter.createCaller(
-            createInnerTRPCContextMockSession([
-                UserRoleTitles.ZADAVATEL_DOTAZNIKU,
-            ])
-        );
-        const createdTask = await employeeCaller.task.createTask(mockInputTask);
         const clientCaller = appRouter.createCaller(
             createInnerTRPCContextMockSession([UserRoleTitles.KLIENT_PACIENT])
         );
         await expect(
             clientCaller.task.deleteTask({
-                id: createdTask.id,
+                id: faker.string.uuid(),
             })
         ).rejects.toMatchInlineSnapshot("[TRPCError: FORBIDDEN]");
     });
@@ -320,38 +323,28 @@ describe("todo permissions", () => {
     });
 
     it("throws when getting todo not assigned to client/patient", async () => {
-        const employeeCaller = appRouter.createCaller(
-            createInnerTRPCContextMockSession([
-                UserRoleTitles.ZADAVATEL_DOTAZNIKU,
-            ])
+        const clientCtx = createInnerTRPCContextMockSession(
+            [UserRoleTitles.KLIENT_PACIENT],
+            faker.string.uuid()
         );
-        vi.mocked(loadUsers).mockImplementationOnce(async () => {
-            const mockUsers = [
-                {
-                    _id: "12345",
-                    data: { id: "12345" },
-                    created: "",
-                    owner: "",
-                    access: [],
-                    form: "",
-                    roles: [],
-                    metadata: {},
-                },
-            ];
-            return mockUsers;
-        });
-        const createdTask = await employeeCaller.task.createTask({
-            ...mockInputTask,
-            forUserId: "12345",
-        });
-        const clientCaller = appRouter.createCaller(
-            createInnerTRPCContextMockSession(
-                [UserRoleTitles.KLIENT_PACIENT],
-                "123"
-            )
-        );
+        const clientCaller = appRouter.createCaller(clientCtx);
+
+        const mockTaskForDifferentUser = {
+            id: faker.string.uuid(),
+            forUserId: faker.string.uuid(), // different user
+            createdAt: faker.date.past(),
+            updatedAt: faker.date.past(),
+            createdByEmployeeId: faker.string.uuid(),
+            state: TaskState.READY,
+            submissionId: null,
+            description: "",
+            name: "",
+            formId: faker.string.uuid(),
+        };
+
+        prisma.task.findUnique.mockResolvedValueOnce(mockTaskForDifferentUser);
         await expect(() =>
-            clientCaller.task.getTask({ id: createdTask.id })
+            clientCaller.task.getTask({ id: mockTaskForDifferentUser.id })
         ).rejects.toMatchInlineSnapshot("[TRPCError: FORBIDDEN]");
     });
 });
