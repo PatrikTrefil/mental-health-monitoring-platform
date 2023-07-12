ecfe0400390d0d9a898027927fd1f88e346e91de
Patrik Trefil <patrik.trefil@gmail.com>
2023-07-12 10:18:22 +0200

    rm unused env vars from example and TS

--- src/web-app/src/env.mjs
+++ src/web-app/src/env.mjs
@@ -46,8 +46,7 @@ export const productionBuildEnvSchema = allEnvVarsSchema.pick(buildMask);
 export const productionServerEnvSchema = allEnvVarsSchema.omit(buildMask);
 
 // If you need to include more variables for new tests, add it the the mask
-// and make sure to add an example value to the `/.env.test.example` file.
-const testMask = { DATABASE_URL: true };
+const testMask = {};
 
 /**
  * Environment variables needed for testing.
