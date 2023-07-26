bb4440aaa8475c9d73dd9ccc6ebf4025cff1d4cf
Patrik Trefil <patrik.trefil@gmail.com>
2023-07-26 17:53:46 +0200

    simplify and improve form preview

--- src/web-app/src/app/zamestnanec/formular/[formId]/nahled/ClientPreviewFormPage.tsx
+++ src/web-app/src/app/zamestnanec/formular/[formId]/nahled/ClientPreviewFormPage.tsx
@@ -1,41 +1,63 @@
 "use client";
 
+import { formsQuery } from "@/client/queries/formManagement";
 import DynamicFormWithAuth from "@/components/shared/formio/DynamicFormWithAuth";
-import { useState } from "react";
-import {
-    Button,
-    Modal,
-    ModalBody,
-    ModalFooter,
-    ModalHeader,
-    ModalTitle,
-} from "react-bootstrap";
+import { useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import { Alert, Spinner } from "react-bootstrap";
 
 /**
  * Page for previewing form with given form id (from url)
  */
 export default function ClientPreviewFormPage({ formId }: { formId: string }) {
-    const [showModal, setShowModal] = useState(false);
+    const session = useSession();
+
+    const {
+        data: form,
+        isLoading,
+        isError,
+        error,
+    } = useQuery({
+        ...formsQuery.detail(session.data?.user.formioToken!, formId),
+        enabled: !!session.data?.user.formioToken,
+    });
+
+    if (isLoading)
+        return (
+            <div className="position-absolute top-50 start-50 translate-middle">
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Načítání...</span>
+                </Spinner>
+            </div>
+        );
+
+    if (isError) {
+        console.error(error);
+        return (
+            <Alert variant="danger">
+                <Alert.Heading>Něco se pokazilo</Alert.Heading>
+                <p>Načítání náhledu formuláře selhalo.</p>
+            </Alert>
+        );
+    }
+
+    if (form === null)
+        return (
+            <Alert variant="danger">
+                <Alert.Heading>Formulář neexistuje</Alert.Heading>
+                <p>Formulář s ID {formId} neexistuje.</p>
+            </Alert>
+        );
 
     return (
         <>
+            <h1>Náhled formuláře - {form.title}</h1>
             <DynamicFormWithAuth
                 relativeFormPath={`/form/${formId}`}
-                onSubmit={(_s, _fP) => {
-                    setShowModal(true);
+                options={{
+                    readOnly: true,
                 }}
             />
-            <Modal show={showModal}>
-                <ModalHeader>
-                    <ModalTitle>Formulář nebyl odeslán</ModalTitle>
-                </ModalHeader>
-                <ModalBody>
-                    Toto je pouze náhled na formulář a nelze jej odeslat.
-                </ModalBody>
-                <ModalFooter>
-                    <Button onClick={() => setShowModal(false)}>Zavřít</Button>
-                </ModalFooter>
-            </Modal>
         </>
     );
 }
