e5fa44845a6b8fcaf1f651b8a3d8d3a5ad117ba7
Patrik Trefil <patrik.trefil@gmail.com>
2023-08-28 15:28:21 +0200

    react hook form for task creation

--- src/web-app/src/app/zamestnanec/sprava-ukolu/TaskCreationForm.tsx
+++ src/web-app/src/app/zamestnanec/sprava-ukolu/TaskCreationForm.tsx
@@ -3,12 +3,49 @@
 import { formsQuery } from "@/client/queries/formManagement";
 import { usersQuery } from "@/client/queries/userManagement";
 import { trpc } from "@/client/trpcClient";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
-import { FormEventHandler, useMemo, useState } from "react";
-import { Alert, Button, Form, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { useMemo } from "react";
+import {
+    Alert,
+    Button,
+    Card,
+    Form,
+    InputGroup,
+    OverlayTrigger,
+    Tooltip,
+} from "react-bootstrap";
+import { SubmitHandler, useController, useForm } from "react-hook-form";
 import Select from "react-select";
 import { toast } from "react-toastify";
+import { z } from "zod";
+
+const formSchema = z.object({
+    taskName: z.string(),
+    taskDescription: z.string(),
+    taskUserIds: z.string().array(),
+    taskFormId: z.string(),
+    deadline: z
+        .object({
+            taskDueDate: z.string(),
+            taskDueTime: z.string(),
+            taskCanBeCompletedAfterDeadline: z.boolean(),
+        })
+        .refine(
+            (data) => {
+                if (data.taskDueDate !== "" && data.taskDueTime === "")
+                    return false;
+                return true;
+            },
+            {
+                message: "Doplňte čas deadline",
+                path: ["taskDueTime"],
+            }
+        ),
+});
+
+type FormInput = z.infer<typeof formSchema>;
 
 /**
  * Props for {@link TaskCreationForm}.
@@ -81,20 +118,58 @@ export default function TaskCreationForm({ onSettled }: TaskCreationFormProps) {
         },
     });
 
-    const [taskName, setTaskName] = useState("");
-    const [taskDescription, setTaskDescription] = useState<string>("");
-    const [taskUserIds, setTaskUserIds] = useState<string[]>([]);
-    const [taskFormId, setTaskFormId] = useState<string>();
-    const [taskDueDate, setTaskDueDate] = useState<string>("");
-    const [taskDueTime, setTaskDueTime] = useState<string>("");
-    const [
-        taskCanBeCompletedAfterDeadline,
-        setTaskCanBeCompletedAfterDeadline,
-    ] = useState<boolean>(false);
+    const {
+        handleSubmit,
+        formState: { errors },
+        watch,
+        register,
+        control,
+        setValue,
+        getValues,
+    } = useForm<FormInput>({
+        mode: "onTouched",
+        resolver: zodResolver(formSchema),
+    });
 
-    const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-        e.preventDefault();
+    const {
+        field: {
+            value: taskUserIdsValue,
+            onChange: taskUserIdsOnChange,
+            ...restTaskUserIdsField
+        },
+    } = useController<FormInput, "taskUserIds">({
+        name: "taskUserIds",
+        control,
+        defaultValue: [],
+    });
 
+    const {
+        field: {
+            value: taskFormIdValue,
+            onChange: taskFormIdOnChange,
+            ...restTaskFormIdField
+        },
+    } = useController<FormInput, "taskFormId">({ name: "taskFormId", control });
+
+    const {
+        field: {
+            value: canTaskBeCompletedAfterDeadlineValue,
+            onChange: canTaskBeCompletedAfterDeadlineOnChange,
+            ...restCanTaskBeCompletedAfterDeadline
+        },
+    } = useController<FormInput, "deadline.taskCanBeCompletedAfterDeadline">({
+        name: "deadline.taskCanBeCompletedAfterDeadline",
+        defaultValue: false,
+        control,
+    });
+
+    const action: SubmitHandler<FormInput> = ({
+        deadline: { taskCanBeCompletedAfterDeadline, taskDueDate, taskDueTime },
+        taskDescription,
+        taskFormId,
+        taskName,
+        taskUserIds,
+    }) => {
         for (const userId of taskUserIds) {
             let deadline:
                 | { dueDateTime: Date; canBeCompletedAfterDeadline: boolean }
@@ -122,7 +197,7 @@ export default function TaskCreationForm({ onSettled }: TaskCreationFormProps) {
                 name: taskName,
                 description: taskDescription,
                 forUserId: userId,
-                formId: taskFormId!, // formId is required
+                formId: taskFormId,
                 deadline,
             });
         }
@@ -165,112 +240,154 @@ export default function TaskCreationForm({ onSettled }: TaskCreationFormProps) {
     }
 
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Label htmlFor="nazev-ukolu">Název úkolu</Form.Label>
+        <Form onSubmit={handleSubmit(action)}>
+            <Form.Label htmlFor="task-name">Název úkolu</Form.Label>
             <Form.Control
                 type="text"
-                id="nazev-ukolu"
-                onChange={(e) => setTaskName(e.target.value)}
-                value={taskName}
+                id="task-name"
                 required
+                {...register("taskName")}
             />
-            <Form.Label htmlFor="popis-ukolu">
+            <Form.Label htmlFor="task-description">
                 Popis úkolu <i>(nepovinné)</i>
             </Form.Label>
             <Form.Control
                 type="text"
-                id="popis-ukolu"
-                onChange={(e) => setTaskDescription(e.target.value)}
-                value={taskDescription}
+                id="task-description"
+                {...register("taskDescription")}
             />
-            <Form.Label htmlFor="pro-uzivatele">Pro uživatele</Form.Label>
+            <Form.Label htmlFor="task-user-ids">Pro uživatele</Form.Label>
             <Select
-                id="pro-uzivatele"
+                id="task-user-ids"
                 required
                 options={userOptionsList}
                 isLoading={isLoadingUsers}
                 isSearchable
                 isMulti
                 placeholder="Vyberte uživatele"
+                value={userOptionsList?.filter((item) =>
+                    taskUserIdsValue.includes(item.value)
+                )}
                 onChange={(selected) =>
-                    setTaskUserIds(
+                    taskUserIdsOnChange(
                         Array.from(selected.values()).map((item) => item.value)
                     )
                 }
+                {...restTaskUserIdsField}
             />
-            <Form.Label htmlFor="form-id">Formulář k vyplnění</Form.Label>
+            <Form.Label htmlFor="task-form-id">Formulář k vyplnění</Form.Label>
             <Select
-                id="form-id"
+                id="task-form-id"
                 required
                 options={formOptionsList}
                 isLoading={isLoadingForms}
-                onChange={(newValue) => setTaskFormId(newValue?.value)}
                 placeholder="Vyberte formulář"
-            />
-            <Form.Label htmlFor="deadline-date">
-                Deadline datum <i>(nepovinné)</i>
-            </Form.Label>
-            <Form.Control
-                id="deadline-date"
-                type="date"
-                value={taskDueDate}
-                onChange={(e) => {
-                    if (e.target.value === "") {
-                        setTaskCanBeCompletedAfterDeadline(false);
-                        setTaskDueTime("");
-                    } else if (taskDueDate === "") {
-                        setTaskDueTime("23:59");
-                    }
-                    setTaskDueDate(e.target.value);
+                value={
+                    taskFormIdValue
+                        ? formOptionsList?.find(
+                              (item) => item.value === taskFormIdValue
+                          )
+                        : taskFormIdValue
+                }
+                onChange={(selected) => {
+                    if (typeof selected === "object")
+                        taskFormIdOnChange(selected?.value);
+                    else taskFormIdOnChange(selected);
                 }}
-                min={new Date().toISOString().split("T")[0]}
+                {...restTaskFormIdField}
             />
-            <Form.Label htmlFor="deadline-time">Deadline čas</Form.Label>
-            <OverlayTrigger
-                overlay={
-                    <Tooltip>
-                        Toto pole lze vyplnit pouze pokud je nastaven deadline.
-                    </Tooltip>
-                }
-                trigger={taskDueDate === "" ? ["hover", "focus"] : []}
-                placement="top"
-            >
-                <Form.Control
-                    id="deadline-time"
-                    type="time"
-                    value={taskDueTime}
-                    disabled={taskDueDate === ""}
-                    onChange={(e) => setTaskDueTime(e.target.value)}
-                    min={
-                        taskDueDate === new Date().toISOString().split("T")[0] // current time
-                            ? `${new Date().getHours()}:${new Date().getMinutes()}`
-                            : undefined
-                    }
-                />
-            </OverlayTrigger>
-            <Form.Check
-                className="mt-2"
-                type="switch"
-                label={
-                    <OverlayTrigger
-                        overlay={
-                            <Tooltip>
-                                Toto pole lze vyplnit pouze pokud je nastaven
-                                deadline.
-                            </Tooltip>
+            <Card className="mt-3 mb-1">
+                <Card.Body>
+                    <Card.Title>
+                        Deadline <i>(nepovinné)</i>
+                    </Card.Title>
+                    <Form.Label htmlFor="task-due-date">Datum</Form.Label>
+                    <Form.Control
+                        id="task-due-date"
+                        type="date"
+                        {...register("deadline.taskDueDate", {
+                            deps: ["deadline.taskDueTime"],
+                            onChange: (e) => {
+                                if (getValues("deadline.taskDueDate") === "") {
+                                    setValue("deadline.taskDueTime", "");
+                                    setValue(
+                                        "deadline.taskCanBeCompletedAfterDeadline",
+                                        false
+                                    );
+                                } else if (
+                                    getValues("deadline.taskDueTime") === ""
+                                )
+                                    setValue("deadline.taskDueTime", "23:59");
+                            },
+                        })}
+                        min={new Date().toISOString().split("T")[0]}
+                    />
+                    <Form.Label htmlFor="deadline-time">Čas</Form.Label>
+                    <InputGroup hasValidation>
+                        <OverlayTrigger
+                            overlay={
+                                <Tooltip>
+                                    Toto pole lze vyplnit pouze pokud je
+                                    nastaven deadline.
+                                </Tooltip>
+                            }
+                            trigger={
+                                watch("deadline.taskDueDate") === ""
+                                    ? ["hover", "focus"]
+                                    : []
+                            }
+                            placement="top"
+                        >
+                            <Form.Control
+                                id="deadline-time"
+                                type="time"
+                                {...register("deadline.taskDueTime")}
+                                disabled={watch("deadline.taskDueDate") === ""}
+                                min={
+                                    watch("deadline.taskDueDate") ===
+                                    new Date().toISOString().split("T")[0] // current time string
+                                        ? `${new Date().getHours()}:${new Date().getMinutes()}`
+                                        : undefined
+                                }
+                                isInvalid={!!errors.deadline}
+                            />
+                        </OverlayTrigger>
+                        <Form.Control.Feedback type="invalid">
+                            {errors.deadline?.taskDueTime?.message}
+                        </Form.Control.Feedback>
+                    </InputGroup>
+                    <Form.Check
+                        className="mt-2"
+                        type="switch"
+                        label={
+                            <OverlayTrigger
+                                overlay={
+                                    <Tooltip>
+                                        Toto pole lze vyplnit pouze pokud je
+                                        nastaven deadline.
+                                    </Tooltip>
+                                }
+                                trigger={
+                                    watch("deadline.taskDueDate") === ""
+                                        ? ["hover", "focus"]
+                                        : []
+                                }
+                                placement="top"
+                            >
+                                <span>Může být splněn i po deadline?</span>
+                            </OverlayTrigger>
                         }
-                        trigger={taskDueDate === "" ? ["hover", "focus"] : []}
-                        placement="top"
-                    >
-                        <span>Může být splněn i po deadline?</span>
-                    </OverlayTrigger>
-                }
-                checked={taskCanBeCompletedAfterDeadline}
-                disabled={taskDueDate === ""}
-                onChange={(e) =>
-                    setTaskCanBeCompletedAfterDeadline(e.target.checked)
-                }
-            />
+                        disabled={watch("deadline.taskDueDate") === ""}
+                        checked={canTaskBeCompletedAfterDeadlineValue}
+                        onChange={(e) =>
+                            canTaskBeCompletedAfterDeadlineOnChange(
+                                e.target.checked
+                            )
+                        }
+                        {...restCanTaskBeCompletedAfterDeadline}
+                    />
+                </Card.Body>
+            </Card>
             <Form.Control
                 type="submit"
                 value={createTask.isLoading ? "Vytváření..." : "Vytvořit úkol"}
