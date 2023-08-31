1d78effc626fa24724cdf9df0bba0b036f3213af
Patrik Trefil <patrik.trefil@gmail.com>
2023-08-31 14:20:39 +0200

    add missing docstring

--- src/web-app/src/app/zamestnanec/sprava-formularu/toolbar-items/CreateFormToolbarItem.tsx
+++ src/web-app/src/app/zamestnanec/sprava-formularu/toolbar-items/CreateFormToolbarItem.tsx
@@ -5,7 +5,7 @@ import { useSession } from "next-auth/react";
 import { Button } from "react-bootstrap";
 
 /**
- *
+ * Toolbar item for creating new forms.
  */
 export default function CreateFormToolbarItem() {
     const { data } = useSession();
